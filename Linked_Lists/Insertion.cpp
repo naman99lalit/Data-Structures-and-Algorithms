d0b54f548470a7e05ce34d6536610b2051582813
namanlalit <naman.lalit@gmail.com>
2019-02-04 22:06:57 +0530

    Insertion Completed

--- Linked_Lists/Insertion.cpp
+++ Linked_Lists/Insertion.cpp
@@ -8,10 +8,10 @@ struct Node
 	struct Node* next;
 };
 struct Node* head;
-int countnodes(int roll,char s[])
+int countnodes(struct Node** head)
 {
-	struct Node* temp=head;
-	int count=0;
+	struct Node* temp=*head;
+	int count=1;
 	while(temp->next!=NULL)
 	{
 		count++;
@@ -26,39 +26,53 @@ struct Node* getnode(int roll,char name[])
 	strcpy(p->name,name);
 	return p;
 }
-void insert_begin(int roll,char name[])
+void insert_begin(struct Node** head,int roll,char name[])
 {
     struct Node* p=getnode(roll,name);
-    if(head==NULL)
-    {
-    	head=p;
-    	return;
-	}
-	p->next=head;
-	head=p;	
+	p->next=*head;
+	*head=p;	
 }
-void insert_end(int roll,char name[])
+void insert_end(struct Node** head,int roll,char name[])
 {
    struct Node* p=getnode(roll,name);
-   if(head==NULL)//list is empty
+   p->next=NULL;
+   if(*head==NULL)//list is empty
    {
-     head=p;
-	 p->next=NULL;	
+     *head=p;
+	 return;	
    }
    else
    {
-   	struct Node* temp=head;
+   	struct Node* temp=*head;
    	while(temp->next!=NULL)
    	{
    		temp=temp->next;
 	}
 	temp->next=p;
+	return;
    }	
 }
-void display()
+void insert_between(struct Node* head,int roll,char name[],int k)
 {
-	struct Node* p;
-	p=head;
+	if(head->next==NULL)
+	{
+	  cout<<"You need to enter atleast one rollno in the list\n";
+	  return;	
+	}
+	else
+	{
+	   struct Node* temp=head;
+	   while(temp->rollno!=k)
+	   {
+	   	temp=temp->next;
+	   }
+	   struct Node* p=getnode(roll,name);
+	   p->next=temp->next;
+	   temp->next=p;	
+	}
+}
+void display(struct Node* p)
+{
 	while(p!=NULL)
 	{
 		cout<<"Roll No: "<<p->rollno<<"  "<<"Name: "<<p->name<<endl;
@@ -73,7 +87,7 @@ int main()
 	cout<<"Inserting Data In A Linked List\n";
 	while(1)
 	{
-		cout<<"Press 1 to insert data in the beginning of Linked List\n"<<"Press 2 to insert data at the end of the linked List\n"<<"Press 3 to insert a number in between of the Linked List\n"<<"Press 4 to Display the List\n"<<"Press 5 to Exit\n";
+		cout<<"Press 1 to insert data in the beginning of Linked List\n"<<"Press 2 to insert data at the end of the linked List\n"<<"Press 3 to insert a number in between of the Linked List\n"<<"Press 4 to Display the List\n"<<"Press 5 to Count the no of nodes in the list\n"<<"Press 6 to Exit\n";
         cin>>c;
         if(c==1)
         {
@@ -83,7 +97,7 @@ int main()
         	cin>>name;
 			cout<<"Enter the Roll no of the student\n";
 			cin>>rollno;
-			insert_begin(rollno,name);
+			insert_begin(&head,rollno,name);
 		}
 		else if(c==2)
 		{
@@ -93,23 +107,30 @@ int main()
         	cin>>name;
 			cout<<"Enter the Roll no of the student\n";
 			cin>>rollno;
-			insert_end(rollno,name);
+			insert_end(&head,rollno,name);
 		}
 		else if(c==3)
 		{
 			char name[100];
-        	int rollno;
+        	int rollno,k;
         	cout<<"Enter the name of the student\n";
         	cin>>name;
 			cout<<"Enter the Roll no of the student\n";
 			cin>>rollno;
-			//insert_between(rollno,name);
+			cout<<"Enter the roll no after which you want to add the element\n";
+			cin>>k;
+			insert_between(head,rollno,name,k);
 		}
 		else if(c==4)
 		{
-			display();
+			display(head);
 		}
 		else if(c==5)
+		{
+			int m=countnodes(&head);
+			cout<<m<<endl;
+		}
+		else
 		{
 			break;
 		}
